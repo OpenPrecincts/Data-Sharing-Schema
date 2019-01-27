6d8204ea82b1eba0549d7bc2e9e4eb1bb1c1325d
Michal Migurski <mike@teczno.com>
2019-01-26 23:22:20 -0800

    Simplified feed validation script

--- OpenPrecincts/validate.py
+++ OpenPrecincts/validate.py
@@ -19,114 +19,93 @@ def find_matching_feed_file(zf, role, expected_name):
     logging.debug('Found {} file: {}'.format(role, found_name))
     return found_name
 
-def find_elections(zf):
-    '''
-    '''
-    return find_matching_feed_file(zf, 'elections', 'elections.csv')
-
-def find_districts(zf):
-    '''
-    '''
-    return find_matching_feed_file(zf, 'districts', 'districts.csv')
-
-def find_precincts(zf):
-    '''
-    '''
-    return find_matching_feed_file(zf, 'precincts', 'precincts.csv')
-
-def find_sources(zf):
-    '''
-    '''
-    return find_matching_feed_file(zf, 'sources', 'sources.csv')
-
-def find_shapes(zf):
-    '''
-    '''
-    return find_matching_feed_file(zf, 'shapes', 'shapes.shp')
-
 def validate_dataframe_columns(df, path, fields):
     '''
     '''
-    is_valid = True
     expected_fields = set(fields)
     found_fields = set(df.columns)
+
+    matching_fields = expected_fields & found_fields
+    missing_fields = expected_fields - found_fields
     
-    for matching_field in (expected_fields & found_fields):
+    for matching_field in matching_fields:
         logging.debug('Found {} field in {}'.format(matching_field, path))
-        is_valid &= True
     
-    for missing_field in (expected_fields - found_fields):
+    for missing_field in missing_fields:
         logging.error('Missing {} field in {}'.format(missing_field, path))
-        is_valid = False
     
-    return is_valid
+    if missing_fields:
+        return False
+    
+    return True
 
 def validate_feed_textfile_fields(zf, path, fields):
     '''
     '''
+    if path is None:
+        return False
+    
     dataframe = pandas.read_csv(zf.open(path))
     return validate_dataframe_columns(dataframe, path, fields)
 
-def validate_feed_elections(zf, elections_path):
+def validate_feed_elections(zf):
     '''
     '''
-    return validate_feed_textfile_fields(zf, elections_path,
+    return validate_feed_textfile_fields(zf,
+        find_matching_feed_file(zf, 'elections', 'elections.csv'),
         ('election_id', 'state_id', 'election_date', 'election_type'))
 
-def validate_feed_districts(zf, districts_path):
+def validate_feed_districts(zf):
     '''
     '''
-    return validate_feed_textfile_fields(zf, districts_path,
+    return validate_feed_textfile_fields(zf,
+        find_matching_feed_file(zf, 'districts', 'districts.csv'),
         ('district_id', 'state_id', 'district_plan', 'district_name',
         'chamber_name', 'shape_id', 'source_id'))
 
-def validate_feed_precincts(zf, precincts_path):
+def validate_feed_precincts(zf):
     '''
     '''
-    return validate_feed_textfile_fields(zf, precincts_path,
+    return validate_feed_textfile_fields(zf,
+        find_matching_feed_file(zf, 'precincts', 'precincts.csv'),
         ('election_id', 'district_id', 'county_id', 'county_name',
         'precinct_name', 'shape_id', 'source_id'))
 
-def validate_feed_sources(zf, sources_path):
+def validate_feed_sources(zf):
     '''
     '''
-    return validate_feed_textfile_fields(zf, sources_path,
+    return validate_feed_textfile_fields(zf,
+        find_matching_feed_file(zf, 'sources', 'sources.csv'),
         ('source_id', 'source_name', 'source_url'))
 
-def validate_feed_shapes(zf, shapes_path):
+def validate_feed_shapes(zf):
     '''
     '''
+    shapes_path = find_matching_feed_file(zf, 'shapes', 'shapes.shp')
+    
+    if shapes_path is None:
+        return False
+    
     dataframe = geopandas.read_file('/vsizip/{}/{}'.format(zf.filename, shapes_path))
     return validate_dataframe_columns(dataframe, shapes_path, ('shape_id', 'geometry'))
 
 def validate_feed_file(feed_path):
     '''
     '''
-    path_checks = [
-        (find_elections, validate_feed_elections),
-        (find_districts, validate_feed_districts),
-        (find_precincts, validate_feed_precincts),
-        (find_sources, validate_feed_sources),
-        (find_shapes, validate_feed_shapes),
-        ]
-    
     logging.info('Validating {}'.format(feed_path))
-    is_valid = True
 
+    all_checks = (validate_feed_elections, validate_feed_districts,
+        validate_feed_precincts, validate_feed_sources, validate_feed_shapes)
+    
     with zipfile.ZipFile(feed_path) as zf:
-        for (find_path, validate_path) in path_checks:
-            path = find_path(zf)
-            if path:
-                is_valid &= validate_path(zf, path)
-            else:
-                is_valid = False
+        check_results = [one_check(zf) for one_check in all_checks]
     
-    if is_valid:
-        logging.info('Feed {} is valid'.format(feed_path))
-    else:
+    if False in check_results:
         logging.error('Feed {} is not valid'.format(feed_path))
+        return False
     
-    return is_valid
+    logging.info('Feed {} is valid'.format(feed_path))
+    return True
 
 def main():
     parser = argparse.ArgumentParser(description='Validate feed.')
@@ -134,7 +113,7 @@ def main():
     parser.add_argument('--verbose', '-v', help='Print lots of debug info',
         dest='loglevel', action='store_const', const=logging.DEBUG, default=logging.INFO)
     parser.add_argument('--quiet', '-q', help='Print nothing but errors',
-        dest='loglevel', action='store_const', const=logging.WARNING, default=logging.INFO)
+        dest='loglevel', action='store_const', const=logging.ERROR, default=logging.INFO)
     
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
